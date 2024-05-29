a9dfea0a1e883b738ca650d42f9f0e91834cdb36
Raju Jena <jenaraju13@gmail.com>
2024-05-29 23:26:39 +0530

    changes made

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('The Man Behind Story is Unbealivable. Raju know about him'));
+app.get('/', (req, res) => res.send('The Man Behind Story is Unbealivable.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
